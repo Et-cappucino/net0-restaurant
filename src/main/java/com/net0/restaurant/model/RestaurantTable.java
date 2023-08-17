2606f6a77062773d879fd9a459d1434dceb01998
Emin Ter-Mkrtchyan <emin.termkrtchyan@gmail.com>
2023-08-17 17:06:02 +0400

    Update RestaurantTable entity with fields

--- src/main/java/com/net0/restaurant/model/RestaurantTable.java
+++ src/main/java/com/net0/restaurant/model/RestaurantTable.java
@@ -1,4 +1,33 @@
 package com.net0.restaurant.model;
 
+import com.net0.restaurant.model.enums.Area;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
 public class RestaurantTable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "table_name", nullable = false)
+    private String name;
+
+    @Column(name = "number_of_seats", nullable = false)
+    private int seats;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "area", nullable = false)
+    private Area area;
+
+    @Column(name = "is_reservable", nullable = false)
+    private boolean isReservable;
 }
--- src/main/java/com/net0/restaurant/repository/RestaurantTableRepository.java
+++ src/main/java/com/net0/restaurant/repository/RestaurantTableRepository.java
@@ -1,4 +1,9 @@
 package com.net0.restaurant.repository;
 
-public interface RestaurantTableRepository {
+import com.net0.restaurant.model.RestaurantTable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RestaurantTableRepository extends JpaRepository<RestaurantTable, Long> {
 }
