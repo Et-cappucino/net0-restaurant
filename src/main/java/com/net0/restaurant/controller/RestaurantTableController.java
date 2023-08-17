59ed469235ee6cfe91c97f3d3455a31e3604b14b
Emin Ter-Mkrtchyan <emin.termkrtchyan@gmail.com>
2023-08-17 18:38:33 +0400

    Update RestaurantTable entity and controller with api documentation

--- src/main/java/com/net0/restaurant/controller/RestaurantTableController.java
+++ src/main/java/com/net0/restaurant/controller/RestaurantTableController.java
@@ -2,6 +2,12 @@ package com.net0.restaurant.controller;
 
 import com.net0.restaurant.model.RestaurantTable;
 import com.net0.restaurant.service.RestaurantTableService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Tag(name = "Restaurant-Table-Controller")
 @RestController
 @RequestMapping("/api/tables")
 @RequiredArgsConstructor
@@ -23,24 +30,49 @@ public class RestaurantTableController {
 
     private final RestaurantTableService restaurantTableService;
 
+    @Operation(summary = "Get all tables")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "All Tables Found",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = RestaurantTable.class)) })
+    })
     @GetMapping
     public ResponseEntity<List<RestaurantTable>> getAllTables() {
         var body = restaurantTableService.findAllTables();
         return ResponseEntity.ok(body);
     }
 
+    @Operation(summary = "Get a table by ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Table Found",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = RestaurantTable.class)) }),
+            @ApiResponse(responseCode = "404", description = "Table not found", content = @Content) })
     @GetMapping("/{id}")
     public ResponseEntity<RestaurantTable> getTable(@PathVariable Long id) {
         var body = restaurantTableService.findOne(id);
         return ResponseEntity.ok(body);
     }
 
+    @Operation(summary = "Add a new table")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Table Created",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = RestaurantTable.class)) }),
+            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content) })
     @PostMapping
     public ResponseEntity<RestaurantTable> addTable(@RequestBody RestaurantTable table) {
         var body = restaurantTableService.createTable(table);
         return new ResponseEntity<>(body, HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Edit table details by ID with a provided new one")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Table details edited",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = RestaurantTable.class)) }),
+            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Table not found", content = @Content)})
     @PutMapping("/{id}")
     public ResponseEntity<RestaurantTable> editTableDetails(@PathVariable Long id,
                                                             @RequestBody RestaurantTable table) {
@@ -48,6 +80,10 @@ public class RestaurantTableController {
         return ResponseEntity.ok(body);
     }
 
+    @Operation(summary = "Delete a table by ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Table Deleted"),
+            @ApiResponse(responseCode = "404", description = "Table not found", content = @Content) })
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteTable(@PathVariable Long id) {
         restaurantTableService.deleteTable(id);
--- src/main/java/com/net0/restaurant/model/RestaurantTable.java
+++ src/main/java/com/net0/restaurant/model/RestaurantTable.java
@@ -1,6 +1,7 @@
 package com.net0.restaurant.model;
 
 import com.net0.restaurant.model.enums.Area;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -16,18 +17,23 @@ public class RestaurantTable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(requiredMode = Schema.RequiredMode.REQUIRED)
     private Long id;
 
+    @Schema(requiredMode = Schema.RequiredMode.REQUIRED)
     @Column(name = "table_name", nullable = false)
     private String name;
 
+    @Schema(requiredMode = Schema.RequiredMode.REQUIRED)
     @Column(name = "number_of_seats", nullable = false)
     private int seats;
 
+    @Schema(requiredMode = Schema.RequiredMode.REQUIRED)
     @Enumerated(EnumType.STRING)
     @Column(name = "area", nullable = false)
     private Area area;
 
+    @Schema(requiredMode = Schema.RequiredMode.REQUIRED)
     @Column(name = "is_reservable", nullable = false)
     private boolean isReservable;
 }
